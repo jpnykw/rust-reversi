b539becebb02982ab8ee701abde1abaf9064fe8f
JPNYKW <ilikegamesme@gmail.com>
2019-11-05 00:26:45 +0900

    ✨ Put stone to any pos

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 use std::io;
 
 fn main() {
+    // ターン制御
+    let mut isBlackTurn = true;
+
+    // ゲームの盤面を初期化する
+    let mut board: [[i32; 8]; 8] = [[0; 8]; 8];
+    board[3][3] = 1;
+    board[3][4] = 2;
+    board[4][3] = 2;
+    board[4][4] = 1;
+
     loop {
+        if isBlackTurn {
+            println!("Turn: Black");
+        } else {
+            println!("Turn: White");
+        }
+
         // ユーザーの座標入力
         let mut x = String::new();
         let mut y = String::new();
@@ -10,6 +26,19 @@ fn main() {
         println!("Enter Y pos to put.");
         io::stdin().read_line(&mut y).expect("Failed to read line.");
 
-        println!("You entered ({}, {})", x.trim(), y.trim());
+        let x_pos: usize = x.trim().parse().unwrap();
+        let y_pos: usize = y.trim().parse().unwrap();
+
+        // 石を置く
+        if isBlackTurn {
+            board[y_pos][x_pos] = 2;
+        } else {
+            board[y_pos][x_pos] = 1;
+        }
+
+        println!("{:?}", board);
+
+        println!("\n");
+        isBlackTurn = !isBlackTurn;
     }
 }
