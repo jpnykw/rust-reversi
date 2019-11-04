fd7e7f5fdd846711fae2a6f021a1fa0904ea78a4
JPNYKW <ilikegamesme@gmail.com>
2019-11-04 22:03:15 +0900

    ✨ Get user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // ユーザーの座標入力
+        let mut x = String::new();
+        let mut y = String::new();
+        println!("Enter X pos to put.");
+        io::stdin().read_line(&mut x).expect("Failed to read line.");
+        println!("Enter Y pos to put.");
+        io::stdin().read_line(&mut y).expect("Failed to read line.");
+
+        println!("You entered ({}, {})", x.trim(), y.trim());
+    }
 }
