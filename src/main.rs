1fb8047d9d5eff2675404a516857eca2016fcb4f
JPNYKW <ilikegamesme@gmail.com>
2019-11-05 16:09:04 +0900

    📌 Released version 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::io;
 
-// 石の数を数える
 fn count_stones(_board: [[usize; 8]; 8]) -> [usize; 2] {
     let mut stones: [usize; 2] = [0; 2];
 
@@ -15,12 +14,8 @@ fn count_stones(_board: [[usize; 8]; 8]) -> [usize; 2] {
     return stones;
 }
 
-// ひっくり返したあとの盤面を返す
 fn get_reversed_board(_x: usize, _y: usize, _stone: usize, _board: [[usize; 8]; 8]) -> [[usize; 8]; 8] {
-    // println!("To: ({}, {}), Stone: {}", _x, _y, if _stone == 1 { "WHITE" } else { "BLACK" });
-
     let opponent_stone = if _stone == 1 { 2 } else { 1 };
-    // let mut new_board: [[usize; 8]; 8] = [[0; 8]; 8];
     let dx: [i32; 8] = [0, -1, -1, -1,  0,  1, 1, 1];
     let dy: [i32; 8] = [1,  1,  0, -1, -1, -1, 0, 1];
     let mut new_board = _board;
@@ -30,7 +25,6 @@ fn get_reversed_board(_x: usize, _y: usize, _stone: usize, _board: [[usize; 8]; 
         let mut _y_pos = _y as i32 + dy[id];
         if _x_pos < 0 || _x_pos > 7 || _y_pos < 0 || _y_pos > 7 { continue; }
 
-        // 相手の石が対象先に存在する場合に探査を始める
         if _board[_y_pos as usize][_x_pos as usize] == opponent_stone {
             let mut flag = true;
             let mut count_max = 0;
@@ -49,7 +43,6 @@ fn get_reversed_board(_x: usize, _y: usize, _stone: usize, _board: [[usize; 8]; 
             }
 
             if flag {
-                // println!("OK");
                 _x_pos = _x as i32;
                 _y_pos = _y as i32;
 
@@ -59,9 +52,6 @@ fn get_reversed_board(_x: usize, _y: usize, _stone: usize, _board: [[usize; 8]; 
                     new_board[_y_pos as usize][_x_pos as usize] = _stone;
                 }
             }
-
-            // if _x_pos < 0 || _x_pos > 7 || _y_pos < 0 || _y_pos > 7 { continue; }
-            // println!("OK");
         }
     }
 
@@ -75,10 +65,7 @@ fn alert() {
 }
 
 fn main() {
-    // ターン制御
     let mut is_black_turn = true;
-
-    // ゲームの盤面を初期化する
     let mut board: [[usize; 8]; 8] = [[0; 8]; 8];
     board[3][3] = 1;
     board[3][4] = 2;
@@ -86,22 +73,17 @@ fn main() {
     board[4][4] = 1;
 
     loop {
-        // 石の数を表示
         let stones = count_stones(board);
-        println!("\nStones:\n - Black: {}\n - White: {}", stones[0], stones[1]);
-
-        // ターンの表示
+        println!("\nStones:\n - White: {}\n - Black: {}", stones[0], stones[1]);
         println!("\nTurn: {}", if is_black_turn { "Black" } else { "White" });
-
-        // 盤面の描画(CUI)
         println!("\n  0 1 2 3 4 5 6 7");
+
         for y in 0..8 {
             let mut display: String = y.to_string();
             for x in 0..8 { display += if board[y][x] == 1 { " W" } else if board[y][x] == 2 { " B" } else { " ." }; }
             println!("{}", display);
         }
 
-        // ユーザーの座標入力
         let mut x = String::new();
         let mut y = String::new();
         println!("\nEnter X pos to put.");
@@ -116,12 +98,11 @@ fn main() {
         let y_id: usize = y_pos as usize;
 
         if board[y_id][x_id] > 0 {
-            // 既に石がある場所
             alert();
         } else {
             let stone: usize = if is_black_turn { 2 } else { 1 };
+
             if board == get_reversed_board(x_id, y_id, stone, board) {
-                // 周りにひっくり返せる石がない
                 alert();
             } else {
                 board[y_id][x_id] = stone;
